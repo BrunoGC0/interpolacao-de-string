dc99a2a10361b6ef3e017ad7f84f0c789e3b8c93
Bruno Gonçalves <brunogcp28@gmail.com>
2024-07-10 14:35:32 -0400

    Aula 04 JS avançado - Interpolação de string

---
+++ index.js
@@ -0,0 +1,14 @@
+// const imprimindoInformacoesPessoais = (primeiroNome, UltimoNome, idade) => {
+//     return "Primeiro Nome: " + primeiroNome + ", Último Nome: " + UltimoNome + ", Idade: " + idade
+// }
+
+// console.log(imprimindoInformacoesPessoais ("Roberto", "Dias", 33))
+
+const imprimindoInformacoesPessoais = (primeiroNome, UltimoNome, idade) => {
+    return `Primeiro Nome: ${primeiroNome}, Último Nome: ${UltimoNome}, Idade: ${idade}`
+}
+
+//console.log(imprimindoInformacoesPessoais ("Roberto", "Dias", 33))
+//console.log(`${imprimindoInformacoesPessoais("Roberto", "Dias", 33)}, ele é um DevQuester`)
+
+console.log(`Soma ${10 + 20}`)
